4ef153ecb56013dc244a10594c3965d3d47a454e
nicolaschillemi <nicolaschillemi@hotmail.com>
2019-04-29 19:53:33 -0300

    Ejercicio 3 guia de Programacion 1

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    char nombre[20];
+
+    printf("Cual es tu nomnre?\n ");
+    scanf("%s",nombre);
+    printf("Buen dia\n %s",nombre);
+
+
+    return 0;
+}
